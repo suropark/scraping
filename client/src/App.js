62710c611c4eb29942826968ad2afba90d237a34
suroPark <xoopp5818@gmail.com>
2021-03-12 16:58:02 +0900

    [ 추가 ] google sheets 데이터 가져오기

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Tabletop from "tabletop";
+import axios from "axios";
+import dotenv from "dotenv";
+function App() {
+  dotenv.config();
+  const [values, setvalues] = useState();
+  const [fetch, setfetch] = useState(false);
+  console.log(process.env.REACT_APP_API_KEY);
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_API_KEY)
+      .then((res) => setvalues(res.data.values))
+      .then((res) => setfetch(true));
+  }, []);
+  const render = (values) => {
+    return values.map((value) => {
+      return <div>{`${value[0]}  ${value[1]}    ${value[2]} `}</div>;
+    });
+  };
+
+  return <div>{fetch ? render(values) : <h1>아직</h1>}</div>;
+}
+
+export default App;
