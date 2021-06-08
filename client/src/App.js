aa233892ee5ffc74d802fa2949e28057eda6a3b2
suropark <52706443+suropark@users.noreply.github.com>
2021-06-08 21:48:14 +0900

    기타 : prettierrc 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
-import Tabletop from "tabletop";
-import axios from "axios";
-import dotenv from "dotenv";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
 function App() {
   dotenv.config();
   const [values, setvalues] = useState();
